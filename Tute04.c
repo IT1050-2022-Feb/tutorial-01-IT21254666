3ca7543dc4d17e722f07e03af9608fad47e4b388
IT21254666 <99467345+IT21254666@users.noreply.github.com>
2022-02-11 17:00:23 +0000

    Completed 4th exercise.

--- Tute04.c
+++ Tute04.c
@@ -5,6 +5,9 @@ Implement the three functions minimum(), maximum() and multiply() below the main
 Do not change the code given in the main() function when you are implementing your solution.*/
 
 #include <stdio.h>
+int minimum(int n1, int n2);
+int maximum(int n3, int n4);
+int multiply(int n5, int n6);
 
 int main() {
    int no1, no2;
@@ -17,3 +20,42 @@ int main() {
    printf("%d ", multiply(no1, no2));
    return 0;
 }
+int minimum(int n1, int n2)//first function
+{
+  int min = 0;
+
+  if(n1 > n2)
+  {
+    min = n2;
+  }
+  else
+  {
+    min = n1;
+  }
+  return min;
+}
+
+int maximum(int n3, int n4)//second function
+{
+  int max = 0;
+
+  if(n3 > n4)
+  {
+    max = n3;
+  }
+  else
+  {
+    max = n4;
+  }
+  return max;
+} 
+
+int multiply(int n5, int n6)//third function
+{
+  int prod = 0;
+
+  prod = n5 * n6;
+
+  return prod;
+}
+
